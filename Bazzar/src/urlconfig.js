e2a2483ee52379e6cb5dfeec930b771c77fe41fc
Himanshu Nandan <himanshukarn1999@gmail.com>
2023-07-22 16:04:37 +0530

    first

---
+++ Bazzar/src/urlconfig.js
@@ -0,0 +1,4 @@
+export const api = `https://bazzar-backend-5a17.onrender.com/api/`;
+export const generatePublicUrl = (fileName) => {
+  return `https://bazzar-backend-5a17.onrender.com/public/${fileName}`;
+};
